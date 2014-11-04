e6a2be7a854ecdbb15563dfa71db5384d07ddaa1
Maxim Novik <max.rindon@gmail.com>
2014-11-05 02:50:46 +0300

    Updated to use the latest kafka image

--- start-kafka-shell.sh
+++ start-kafka-shell.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --link stormkafka08plustest_zookeeper_1:zk -i -t wurstmeister/kafka:0.8.1 /bin/bash
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock --link logkafkastorm_zookeeper_1:zk -e HOST_IP=$1 -i -t wurstmeister/kafka:0.8.1.1-1 /bin/bash
